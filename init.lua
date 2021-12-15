ec0e1defef9030c587d7d3865868890e66fe5d22
irishgreencitrus <irishgreencitrus@gmail.com>
2021-12-15 20:07:44 +0000

    Fix Broken Installer

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
-local install_path = fn.stdpath('data') .. '/site/pack/paqs/start/paq-nvim'
+local install_path = vim.fn.stdpath('data') .. '/site/pack/paqs/start/paq-nvim'
 
-if fn.empty(fn.glob(install_path)) > 0 then
-          fn.system({'git', 'clone', '--depth=1', 'https://github.com/savq/paq-nvim.git', install_path})
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+          vim.fn.system({'git', 'clone', '--depth=1', 'https://github.com/savq/paq-nvim.git', install_path})
 end
 require("conf.packages")
 require("conf.dashboard")
