2c10411488a0fd82b922d4f285efa6e2b86095b1
irishgreencitrus <irishgreencitrus@gmail.com>
2021-12-15 20:06:11 +0000

    Auto Install

--- init.lua
+++ init.lua
@@ -1,3 +1,8 @@
+local install_path = fn.stdpath('data') .. '/site/pack/paqs/start/paq-nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+          fn.system({'git', 'clone', '--depth=1', 'https://github.com/savq/paq-nvim.git', install_path})
+end
 require("conf.packages")
 require("conf.dashboard")
 require("conf.settings")
