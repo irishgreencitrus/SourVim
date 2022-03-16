65a32bf6681097b0ba8cdae5a2abcab52138722c
irishgreencitrus <irishgreencitrus@gmail.com>
2022-03-16 11:48:24 +0000

    Initial Commit

---
+++ lua/settings.lua
@@ -0,0 +1,13 @@
+local set = vim.opt
+local setg = vim.g
+
+vim.cmd "colorscheme material"
+set.bg = "dark"
+set.compatible = false
+set.cursorline = true
+set.cursorline.cursorlineopt = "number"
+set.number = true
+set.relativenumber = true
+set.termguicolors = true
+set.showmode = false
+set.mouse = "a"
