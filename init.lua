848b24a9f830faa3f54021b435a0719d669d3641
irishgreencitrus <irishgreencitrus@gmail.com>
2023-04-26 18:51:24 +0100

    Fix weird glitch

--- init.lua
+++ init.lua
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-local install_path = vim.fn.stdpath('data') .. '/site/pack/paqs/start/paq-nvim'
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-	vim.fn.system({'git', 'clone', '--depth=1', 'https://github.com/savq/paq-nvim.git', install_path})
-end
-require("keybind")
-require("packages")
-require("settings")
-=======
 -- bootstrap plugin manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -23,4 +14,3 @@ require("config")
 require("lazy").setup(require("plugins"))
 
 require("keybinds")
->>>>>>> 1b94ce6 (Initial Commit)
